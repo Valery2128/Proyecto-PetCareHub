c776d1d38ff6330c447d4b1b04b5000945232e60
valery2128 <jmorantes32@gmail.com>
2023-08-14 22:07:51 -0500

    Agregado controlador de registro y vista de registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'number_phone' => ['required', 'string', 'min:12', 'confirmed'],
+            'name_user' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
     }
 
@@ -68,6 +70,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'number_phone' => $data['number_phone'],
+            'name_user' => $data['name_user'],
         ]);
     }
 }
